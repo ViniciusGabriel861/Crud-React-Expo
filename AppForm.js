f36b86f12d252834eacc4d5d385332e22cfa1c3f
ViniciusGabriel861 <83305700+ViniciusGabriel861@users.noreply.github.com>
2022-05-16 21:33:24 -0300

    Add files via upload

---
+++ AppForm.js
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+1
+2
+3
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default function AppForm() {
+    return ( < View style = { styles.container } >
+        <
+        Text style = { styles.title } > Item para comprar < /Text> <
+        View style = { styles.inputContainer } >
+        <
+        TextInput style = { styles.input }
+        placeholder = "O que está faltando em casa?"
+        clearButtonMode = "always" / >
+        <
+        TextInput style = { styles.input }
+        placeholder = "Digite a quantidade"
+        keyboardType = { 'numeric' }
+        clearButtonMode = "always" / >
+        <
+        TouchableOpacity style = { styles.button } >
+        <
+        Text style = { styles.buttonText } > Salvar < /Text>  <
+        /TouchableOpacity>  <
+        /View> <
+        StatusBar style = "light" / >
+        <
+        /View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#D93600',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
